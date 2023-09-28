17eb525a67aa456c89860ff609375bcf91083a90
faheeds <42218586+faheeds@users.noreply.github.com>
2023-09-28 10:34:45 -0700

    Update iEat.py

--- iEat.py
+++ iEat.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 
 # Initialize the OpenAI API with your key from Streamlit secrets
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = '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'
 
 def get_recipe_from_openai(dish_name):
     # Construct the message to get a recipe
