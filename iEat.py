10187a86d11b70ef4595b43310f5fdd835fd366e
faheeds <42218586+faheeds@users.noreply.github.com>
2023-09-28 09:25:37 -0700

    Update iEat.py

--- iEat.py
+++ iEat.py
@@ -3,29 +3,36 @@ import streamlit as st
 import requests
 
 def get_recipe_from_openai(dish_name):
-    # This is a pseudo-implementation
-    # For demonstration purposes, we return a mock recipe
-    # In a real-world scenario, you'd call the OpenAI API or another recipe API
-    mock_recipes = {
-        "pasta": [
-            "Pasta Recipe 1: Boil pasta. Add sauce. Serve hot.",
-            "Pasta Recipe 2: Cook pasta al dente. Add olive oil and garlic. Serve."
-        ],
-        "pizza": [
-            "Pizza Recipe 1: Spread dough. Add toppings. Bake at 425°F for 15 mins.",
-            "Pizza Recipe 2: Use thin crust. Add cheese and pepperoni. Bake until crispy."
-        ]
+    # Define your OpenAI API endpoint and authentication
+    endpoint = "https://api.openai.com/search"  # This is a pseudo-endpoint; replace with the actual endpoint
+    headers = {
+        "Authorization": "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"
     }
-    return mock_recipes.get(dish_name, [])
+    # Define your search query
+    query = {
+        "q": f"{dish_name} recipe",
+        "size": 2  # get top 2 results
+    }
+    # Make the API call
+    response = requests.get(endpoint, headers=headers, params=query)
+    # Check for successful response
+    if response.status_code == 200:
+        results = response.json().get("results", [])
+        if not results:
+            return None
+        else:
+            # Extract the top 2 recipes (assuming there's a "recipe" field in the response)
+            top_recipes = [result.get("recipe") for result in results[:2]]
+            return top_recipes
+    else:
+        print("Error:", response.status_code)
+        return None
 
 def main():
     st.title("Recipe Finder")
-    
     dish_name = st.text_input("Enter the name of the dish:")
-
     if st.button("Get Recipe"):
         recipes = get_recipe_from_openai(dish_name)
-        
         if recipes:
             for idx, recipe in enumerate(recipes, 1):
                 st.write(f"Recipe {idx}:\n{recipe}\n")
